ddae0fd1631c7a8467a7f6b031b622cf68c6a86e
Aditi Mishra <itsadm08@outlook.com>
2025-03-15 23:41:15 +0530

    Add files via upload

---
+++ mt.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<windows.h>
+//#include "person.h"
+#include "user.cpp"
+using namespace std;
+#include <string>
+
+int main(){
+
+    manager func;
+    bool run = true;
+
+    while(run){
+        func.menu();
+    }
+
+    return 0;
+}
